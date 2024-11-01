16efe2ccbbf94b118917ebcba583a3b7d08f7b2d
greg <g.coraboeuf84@gmail.com>
2024-11-01 16:07:19 +0100

    First part of the exe

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celcius = ((temp - 32) * 5) / 9;
+  let deg = Math.round(celcius * 10) / 10;
+  return deg;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let far = (temp * 9) / 5 + 32;
+  let deg = Math.round(far * 10) / 10;
+  return deg;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
